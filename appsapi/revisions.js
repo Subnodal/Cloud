415a44210a44a64303f05832f6bd08efd7c85466
James-Livesey <jamesliveg@gmail.com>
2021-10-16 19:29:46 +0100

    Add object diffing for CAAPI revisions system

--- appsapi/revisions.js
+++ appsapi/revisions.js
@@ -58,4 +58,68 @@ namespace("com.subnodal.cloud.appsapi.revisions", function(exports) {
 
         return object;
     };
+
+    exports.diffObjectPaths = function(current, incoming) {
+        var diff = [];
+
+        current.forEach(function(oldPathData) {
+            var oldPathSerialised = JSON.stringify(oldPathData.path);
+            var newPathData = incoming.find((pathData) => JSON.stringify(pathData.path) == oldPathSerialised);
+
+            if (newPathData == undefined) {
+                diff.push({path: oldPathData.path, remove: true});
+            }
+        });
+
+        incoming.forEach(function(newPathData) {
+            var newPathSerialised = JSON.stringify(newPathData.path);
+            var oldPathData = current.find((pathData) => JSON.stringify(pathData.path) == newPathSerialised);
+
+            if (oldPathData != undefined && oldPathData.data == newPathData.data) {
+                return;
+            }
+
+            diff.push(newPathData);
+        });
+
+        return diff;
+    };
+
+    exports.applyDiffToObjectPaths = function(current, diff) {
+        var applied = [];
+        var excludeSerialised = [];
+
+        diff.forEach(function(diffData) {
+            if (diffData.remove) {
+                excludeSerialised.push(JSON.stringify(diffData.path));
+            }
+        });
+
+        current.forEach(function(pathData) {
+            if (excludeSerialised.includes(JSON.stringify(pathData.path))) {
+                return;
+            }
+
+            applied.push(pathData);
+        });
+
+        diff.forEach(function(diffData) {
+            if (diffData.remove) {
+                return;
+            }
+
+            var diffPathSerialised = JSON.stringify(diffData.path);
+            var changeIndex = applied.findIndex((pathData) => JSON.stringify(pathData.path) == diffPathSerialised);
+
+            if (changeIndex >= 0) {
+                applied[changeIndex] = diffData;
+
+                return;
+            }
+
+            applied.push(diffData);
+        });
+
+        return applied;
+    };
 });
